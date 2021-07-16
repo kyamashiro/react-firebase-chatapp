ff5d60762e77be4dd470e9fabab52e81e90e0138
kyamashiro <kyamashiro73@gmail.com>
2021-07-16 23:59:45 +0900

    add material-ui

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import Navbar from "./components/Navbar";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Home} from "./pages/home";
 import {About} from "./pages/about";
+import {Container} from "@material-ui/core";
 
 function App() {
     return (
         <div className="App">
-            <Router>
-                <div>
-                    <Navbar/>
-                    <hr/>
-                    <Route exact path='/' component={Home}/>
-                    <Route path='/About' component={About}/>
-                </div>
-            </Router>
+            <Container maxWidth="xl">
+                <Router>
+                    <div>
+                        <Navbar/>
+                        <hr/>
+                        <Route exact path='/' component={Home}/>
+                        <Route path='/About' component={About}/>
+                    </div>
+                </Router>
+            </Container>
         </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {AppBar, Button, IconButton, Toolbar, Typography} from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
 
 export const Navbar = () => {
     return (
         <div>
-            <Link to="/">Home</Link>
-            <Link to="/About">About</Link>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <MenuIcon/>
+                    </IconButton>
+                    <Link to="/" style={{textDecoration: 'none', color: '#FFF'}}>
+                        <Typography variant="h6">
+                            Home
+                        </Typography>
+                    </Link>
+
+                    <Link to="/About" style={{textDecoration: 'none', color: '#FFF'}}>
+                        <Button color="inherit">About</Button>
+                    </Link>
+                    <Button color="inherit" onClick={() => {
+                        console.log('click')
+                    }}>Login</Button>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 }
